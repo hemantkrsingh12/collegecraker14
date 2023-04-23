f18f71d8e3f45092fce15a5c4444a10132ee1ea7
Aniket PAndey <pandeyaniket85@gmail.com>
2023-04-23 12:04:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import LandingPage from "./LandingPage";
+import RegisterPage from "./RegisterPage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
